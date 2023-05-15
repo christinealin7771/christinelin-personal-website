428398b7a07a04424a3ed3641b880288ec294979
christinelin <christinelin.7771@gmail.com>
2023-05-15 15:08:59 -0400

    change scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,10 +28,10 @@ const Header = () => {
     <header  className={`header ${isScroll? 'scrolled' : ''}`}>
       <div className={`header-content${isScroll? 'header-content-scrolled' : ''}`}>
         <div className = "links">
-            <Link to="/" onClick={window.scrollTo(0,0)}>Home</Link>
-            <Link to="/about-me" onClick={window.scrollTo(0,0)}>About Me</Link>
-            <Link to="/experience" onClick={window.scrollTo(0,0)}>Resume</Link>
-            <Link to="/projects" onClick={window.scrollTo(0,0)}>Projects</Link>
+            <Link to="/" >Home</Link>
+            <Link to="/about-me" >About Me</Link>
+            <Link to="/experience" >Resume</Link>
+            <Link to="/projects">Projects</Link>
             
             {/* <Link to="/WiCSE-Personal-Project/experience">Fun Facts</Link> */}
             
